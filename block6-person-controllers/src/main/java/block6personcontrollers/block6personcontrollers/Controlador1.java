d23ddf8a21e7e144ba042c1a5bf43946ff5ec885
danipozfez <danipozfez@gmail.com>
2023-04-18 09:24:58 +0200

    crea y muestra ciudades

--- block6-person-controllers/src/main/java/block6personcontrollers/block6personcontrollers/Controlador1.java
+++ block6-person-controllers/src/main/java/block6personcontrollers/block6personcontrollers/Controlador1.java
@@ -2,8 +2,12 @@ package block6personcontrollers.block6personcontrollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class Controlador1 {
@@ -11,9 +15,21 @@ public class Controlador1 {
     @Autowired
     Persona persona;
 
+    public List<Ciudad> ciudades = new ArrayList<>();
+
     @PostMapping("/addPersona")
     public Persona addPersona(@RequestHeader String nombre, @RequestHeader String localidad, @RequestHeader int edad) {
         persona = new Persona(nombre,localidad,edad);
         return persona;
+    }
+    @PostMapping("/addCiudad")
+    public Ciudad addCiudad(@RequestBody Ciudad ciudad){
+        ciudad.setNombre(ciudad.getNombre());
+        ciudad.setNumeroHabitantes(ciudad.getNumeroHabitantes());
+        ciudades.add(ciudad);
+        return ciudad;
+    }
+    public List<Ciudad> mostrarCiudades(){
+        return ciudades;
     }
 }
--- block6-person-controllers/src/main/java/block6personcontrollers/block6personcontrollers/Controlador2.java
+++ block6-person-controllers/src/main/java/block6personcontrollers/block6personcontrollers/Controlador2.java
@@ -1,16 +1,27 @@
 package block6personcontrollers.block6personcontrollers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class Controlador2 {
+
+    @Autowired
+    Controlador1 controlador1;
 
     @PostMapping("/getPersona")
     public Persona multiplicaEdadPorDos (@RequestBody Persona persona){
         int edad = persona.getEdad()*2;
         persona.setEdad(edad);
         return persona;
+    }
+    @GetMapping("/getCiudades")
+    public List<Ciudad> mostrarListaCiudades (){
+        return controlador1.mostrarCiudades();
     }
 }
