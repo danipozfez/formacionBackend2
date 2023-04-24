0fb7609ed776816deb796ce34321c23e3dfeb499
danipozfez <danipozfez@gmail.com>
2023-04-24 12:30:43 +0200

    con delete funcionando

---
+++ block7-crud/src/main/java/block7crud/controller/ControladorBorrar.java
@@ -0,0 +1,22 @@
+package block7crud.controller;
+
+import block7crud.application.PersonaService;
+import block7crud.controller.dto.PersonaOutDto;
+import block7crud.domain.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/persona")
+
+public class ControladorBorrar {
+    @Autowired
+    PersonaService personaService;
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String>deletePersonaById(@PathVariable int id){
+        //PersonaService personaService1= (PersonaService) personaService.getPersonaById(id);
+        personaService.deletePersonaById(id);
+        return ResponseEntity.ok().body("la persona con id "+id+" ha sido borrada");
+    }
+}
