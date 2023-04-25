f715e851536909edb3b6fbc42464430d476b6915
danipozfez <danipozfez@gmail.com>
2023-04-25 09:44:56 +0200

    primera tabla creada

--- block7-crud-validation/src/main/java/block7crudvalidation/block7crudvalidation/domain/Persona.java
+++ block7-crud-validation/src/main/java/block7crudvalidation/block7crudvalidation/domain/Persona.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import org.hibernate.annotations.BatchSize;
+
+import java.util.Date;
 
 @Entity
 @Data
@@ -17,6 +22,24 @@ public class Persona {
     @Id
     @GeneratedValue
     int id;
-    @Column
+    @Size(min=6,max=10)
+    @NotNull
     String usuario;
+    @NotNull
+    String password;
+    @NotNull
+    String name;
+    String surName;
+    @NotNull
+    String companyEmail;
+    @NotNull
+    String personalEmail;
+    @NotNull
+    String city;
+    @NotNull
+    Boolean active;
+    @NotNull
+    Date createdDate;
+    String imagenUrl;
+    Date terminationDate;
 }
