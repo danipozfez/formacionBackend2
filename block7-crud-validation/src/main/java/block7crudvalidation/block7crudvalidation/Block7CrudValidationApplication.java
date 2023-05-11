16c4f9fc7212e94ba51d4aeba8a7a896e58ede03
danipozfez <danipozfez@gmail.com>
2023-05-11 13:30:50 +0200

    añadida dependencia

--- block7-crud-validation/src/main/java/block7crudvalidation/block7crudvalidation/Block7CrudValidationApplication.java
+++ block7-crud-validation/src/main/java/block7crudvalidation/block7crudvalidation/Block7CrudValidationApplication.java
@@ -2,7 +2,9 @@ package block7crudvalidation.block7crudvalidation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Block7CrudValidationApplication {
 
