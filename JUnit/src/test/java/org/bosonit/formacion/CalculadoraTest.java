6bdd3737c884ba6b50bef792ba1b84cdca4a385b
danipozfez <danipozfez@gmail.com>
2023-05-12 08:57:05 +0200

    creando test junit

---
+++ JUnit/src/test/java/org/bosonit/formacion/CalculadoraTest.java
@@ -0,0 +1,24 @@
+package org.bosonit.formacion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    @Test
+    void suma() {
+    }
+
+    @Test
+    void resta() {
+    }
+
+    @Test
+    void division() {
+    }
+
+    @Test
+    void multiplicacion() {
+    }
+}
