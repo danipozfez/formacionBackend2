a662a3cd168f75b33909f6398f4d19c66c5f9efd
danipozfez <danipozfez@gmail.com>
2023-05-18 11:34:00 +0200

    empezando bloque 11

--- block11-upload-download-files/block7-crud-validation/src/main/java/block7crudvalidation/block7crudvalidation/repository/AsignaturaRepository.java
+++
@@ -1,7 +0,0 @@
-package block7crudvalidation.block7crudvalidation.repository;
-
-import block7crudvalidation.block7crudvalidation.domain.Asignatura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AsignaturaRepository extends JpaRepository<Asignatura,Integer> {
-}
